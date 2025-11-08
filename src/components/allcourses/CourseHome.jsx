49ed6a6716ebcea1c9b9e576ea506d5f0b66d867
abdu <abdualkerim01@gmail.com>
2025-11-09 00:06:50 +0300

    basic setup for CoursesCard done

--- src/components/allcourses/CourseHome.jsx
+++ src/components/allcourses/CourseHome.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Back from '../common/title/back/Back'
+import CoursesCard from './CoursesCard'
 const CourseHome = () => {
   return (
     <div>
       <Back title='Explore Courses' />
+      <CoursesCard />
     </div>
   )
 }
